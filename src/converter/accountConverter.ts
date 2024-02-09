9e529ae8a4973e04b7242554c752a4608bcd3a29
voruti <45130096+voruti@users.noreply.github.com>
2024-02-09 23:12:55 +0100

    Extract AccountConverter

---
+++ src/converter/accountConverter.ts
@@ -0,0 +1,38 @@
+import { Database } from "sqlite3";
+import { writeFile } from "fs";
+import { stringify } from "csv-stringify";
+
+import { convertDbAccountToCsvAccount, type CsvAccount, type DbAccount } from "../model/accounts";
+
+export class AccountConverter {
+    constructor(private db: Database, private outputFile: string) {}
+
+    public run(): void {
+        this.db.all("SELECT * FROM Account;", [], (err, dbAccounts: DbAccount[]) => {
+            if (err) {
+                console.error("Error reading db:", err.message);
+                return;
+            }
+
+            // convert the rows:
+            const csvAccounts: CsvAccount[] = dbAccounts.map(convertDbAccountToCsvAccount);
+
+            // convert the modified records to CSV:
+            stringify(csvAccounts, { delimiter: ",", header: true }, (err, csvString: string) => {
+                if (err) {
+                    console.error("Error converting to CSV:", err);
+                    return;
+                }
+
+                // write the CSV to the output file:
+                writeFile(this.outputFile, csvString, "utf8", (err) => {
+                    if (err) {
+                        console.error("Error writing to file:", err);
+                    } else {
+                        console.log("Conversion successful. Output written to", this.outputFile);
+                    }
+                });
+            });
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,18 @@
 import { Database } from "sqlite3";
-import { writeFile } from "fs";
-import { stringify } from "csv-stringify";
 
-import { convertDbAccountToCsvAccount, type CsvAccount, type DbAccount } from "./model/account";
+import { AccountConverter } from "./converter/accountConverter";
 
-// input and output file paths:
+// input file path:
 const inputFile: string = "FINANCE_DB";
-const outputFile: string = "output.csv";
 
 // initialize SQLite database:
 const db: Database = new Database(inputFile);
 
-// execute the SQL query:
-db.all("SELECT * FROM Account;", [], (err, dbAccounts: DbAccount[]) => {
+new AccountConverter(db, "accounts.csv").run();
+
+// close the database connection:
+db.close((err) => {
     if (err) {
-        console.error("Error reading db:", err.message);
-        return;
+        console.error("Error closing DB:", err.message);
     }
-
-    // modify the rows:
-    const csvAccounts: CsvAccount[] = dbAccounts.map(convertDbAccountToCsvAccount);
-
-    // convert the modified records to CSV:
-    stringify(csvAccounts, { delimiter: ",", header: true }, (err, output: string) => {
-        if (err) {
-            console.error("Error converting to CSV:", err);
-            return;
-        }
-
-        // write the CSV to the output file:
-        writeFile(outputFile, output, "utf8", (err) => {
-            if (err) {
-                console.error("Error writing to file:", err);
-            } else {
-                console.log("Conversion successful. Output written to", outputFile);
-            }
-        });
-    });
-
-    // close the database connection:
-    db.close((dbErr) => {
-        if (dbErr) {
-            console.error("Error closing DB:", dbErr.message);
-        }
-    });
 });
